1e218d7df714290ae518b64f94f208c786f00b38
zhenghui <jingbo2759@gmai.com>
2015-01-14 22:22:29 +0800

    init

---
+++ base/src/test/java/zhenghui/cache/RedisCacheTest.java
@@ -0,0 +1,19 @@
+package zhenghui.cache;
+
+import org.junit.Test;
+
+/**
+ * User: zhenghui
+ * Date: 15-1-14
+ * Time: ÏÂÎç10:03
+ * œyÔ‡redis ¿Í‘ô¶Ë
+ */
+public class RedisCacheTest {
+
+    @Test
+    public void test1(){
+        Jedis jedis = new Jedis("localhost");
+        jedis.set("foo", "bar");
+        String value = jedis.get("foo");
+    }
+}
